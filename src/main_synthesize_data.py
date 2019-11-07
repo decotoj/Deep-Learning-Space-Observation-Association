776f28c5087e9caef21e03331d9fd8524d42521e
Jake Decoto <45853396+decotoj@users.noreply.github.com>
2019-11-07 11:45:49 -0700

    Add files via upload

---
+++ src/main_synthesize_data.py
@@ -0,0 +1,117 @@
+import twobody as twobody
+import random
+import numpy as np
+import math
+
+#Produce a set of N Optical Observations of RSOs by Randomely Placed Earth Based Observers
+
+#Settings
+N = 100000 #Number of simulated data points in each set (Baseline = 100000)
+nLow = 3 #Low end of range for # of obs of a given RSO
+nHigh = 10 #High end of range for # of obs of a given RSO
+dataTags = ['train', 'val', 'test']
+tf = 3600*12 #Time range of observation is between 0 to tf (seconds)
+StreakLength = 120 #Length of time for each observation from start of collect to end (seconds)
+Pstd = 0.1 #Standard Deviation of Gaussian Error on RSO Position (km) - Introduces Error on Observation Angles
+
+#Constants
+mu = 398600.4418 #Earth Gravitional Constant km^2/s^2
+
+def angleBetweenVectors(v1, v2):
+  return math.acos(np.dot(v1,v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+
+def randomEarthObserver(p):
+
+    while True:
+
+        #Build an Observer Positioned Randomely on Earth (+/- 500 meters from Sea Level)
+        R = random.uniform(6378-0.5, 6378+5)
+        P = [random.random()-0.5 for q in range(3)]
+        m = np.linalg.norm(P)
+        P = [q/m*R for q in P]
+
+        #Ensure Randomly Placed Observer Isn't Looking Through Earth to Observe RSO
+        C = [p[i]-P[i] for i in range(3)] #Vector from Observer to RSO
+        alpha = angleBetweenVectors(C, P) #Angle Off Horizon of Observation Vector (Must be <1.57 radians)
+        if alpha < 1.57:
+            return P
+
+def RandomObservationsN(NumVec, s):
+
+    #Create Random Observations
+    t = []
+    ob = []
+    for i in range(NumVec): 
+         
+         t = random.uniform(0,tf) #Random time between 0 and tf
+         p, _ = s.posvelatt(t)
+         p2, _ = s.posvelatt(t+StreakLength)
+
+         P = randomEarthObserver(p) #Randomly Positioned Earth Observer
+
+         #Add Random Error to RSO Position Knowledge
+         p = [q+random.gauss(0,Pstd) for q in p]
+         p2 = [q+random.gauss(0,Pstd) for q in p2]
+
+         u = [p[0] - P[0], p[1] - P[1], p[2] - P[2]] #RSO Position at Random Index Relative to Observer
+         m = np.linalg.norm(u) 
+         u = [q/m for q in u] #Observation Unit Vector
+
+         u2 = [p2[0] - P[0], p2[1] - P[1], p2[2] - P[2]] #WARNING: Ignores observer movement over streak time interval
+         m = np.linalg.norm(u2)
+         u2 = [q/m for q in u2]
+
+         StreakDirection = [u2[0]-u[0], u2[1]-u[1],u2[2]-u[2]]
+         StreakDirection = [q/StreakLength for q in StreakDirection]
+
+         ob.append([float(t)] + P + u + StreakDirection) #10 params
+        
+    return ob
+
+def randomSatellite():
+
+    s = twobody.TwoBodyOrbit("RSO", mu=mu)   # create an instance
+
+    a = random.uniform(41164,43164) #semi-major axis
+    e = random.uniform(0,0.1)  #eccentricity
+    i = random.uniform(0,20) #inclination
+    LoAN = random.uniform(0,360) #longitude of ascending node
+    AoP = random.uniform(0,360) #argument of perigee
+    MA = random.uniform(0,360) #mean anomaly
+    
+    s.setOrbKepl(0, a, e, i, LoAN, AoP, MA=MA) # define the orbit
+
+    return s
+
+def synthesizeDataset(N, nLow, nHigh, outFile1, outFile2):
+
+    #Create Data
+    obs = []
+    tags = []
+    label = 0
+    while len(obs) < N:
+        n = random.randint(nLow, nHigh)
+        obs = obs + RandomObservationsN(n, randomSatellite()) #Grab n Random Observations
+        tags = tags + [label]*n
+        label += 1
+    obs = obs[0:N] #Cut to Desired Length
+    tags = tags[0:N] #Cut to Desired Length
+    print('obs: ', len(obs), len(tags))
+
+    #Randomize the order of the observations
+    c = list(zip(obs, tags))
+    random.shuffle(c)
+    obs, tags = zip(*c)
+
+    #Write the observation and tag data to files
+    with open(outFile1, 'w') as f:
+        for q in obs:
+            q = [str(r) for r in q]
+            f.write(','.join(q) + '\n')
+    with open(outFile2, 'w') as f:
+        [f.write(str(q) + '\n') for q in tags]
+
+if __name__ == "__main__":
+
+    #Synthesize Train, Validation and Test Data
+    [synthesizeDataset(N, nLow, nHigh, q + '_data.csv', q + '_tags.csv') for q in dataTags]
---
+++ src/main_train_model.py
@@ -0,0 +1,217 @@
+# Experiment Training a Neural Network to Aid Optical Space Observation Association
+# Last Revision: 10/19/2019
+# Authors: Jake Decoto
+
+# Next Steps
+#1) Audit code (main and sythesize) and hone down to minimum
+#2) Make 100% Sure Something is Being Learned (Generalizes)
+#A) Reduce false positive triplets, 1) loss function for doubles penalize false+, 2) separate triplet NN for those identified by doublet NN
+#B) Separate out grading and knowledge of tags into separate callable function (so can be run as unknowing participant)
+#C) Revisit simulated data settings to ensure data is sufficiently real world problem representive
+
+# The following loads prebuilt training and test data consisting of optical observerations
+# of satellites.  A neural network is then trained to identify pairs of optical observations
+# in the labelled training data as being of the same satellite or not.  This network is then
+# used to identify triplets of observations in the test data that are most likely to be of 
+# the same satellite.  No apriori knowledge of physics or orbital mechanics is used.
+
+import torch
+import random
+import numpy as np
+import torch.optim as optim
+from collections import defaultdict
+
+random.seed(457)
+
+#Network and Data Parameters
+LOAD_SAVED_MODEL_FLAG = 1 #1=Load prior saved model, 0=Train model from scratch
+TRAIN_FLAG = 1 #1=TRAIN, 0=EVAL ONLY
+MODEL_FILE = 'model.pt' #Path to saved model file if applicable, also is name of new model file to be saved
+D_in = 416 #Length of Input Vectors after Augmentation (time, observer x-y-z unit vec, observervation x-y-z unit vec, streak direction x-y-z unit vec, observer vector mag)
+H = 416 #Hidden layer(s) dimension
+N = 10000 #Number of training examples to create from loaded data (Baseline = 40000)
+Nval = 1000 #Number of validation examples to create from loaded data (Baseline = 2500)
+NUMEPOCHS = 100 #Number of Training Epochs
+learning_rate = 1e-4
+train_data = 'train_data.csv'
+train_tags = 'train_tags.csv'
+val_data = 'val_data.csv'
+val_tags = 'val_tags.csv'
+
+def augmentAndVectorize(ob):
+
+    #Sort Observations by Epoch and Normalize Epoch
+    ob.sort(key=lambda x: x[0])
+    for q in range(len(ob)):
+        ob[q][0] = ob[q][0] - ob[0][0]
+
+    #Add Derived Parameter from Observation Unit Vector and Streak Vector and Magnitude (416)
+    n = len(ob[0])
+    for z in range(1, len(ob)):
+        for q in [4,5,6,7,8,9,10]:
+            for r in [4,5,6,7,8,9,10]:
+                for s in [0,4,5,6,7,8,9,10]:
+                    ob[z].append((ob[z][q]-ob[z-1][r])/(ob[z][s]-ob[z-1][s]))
+
+    #Vectorize
+    datapoint = []
+    for q in range(len(ob)):
+         datapoint = datapoint + ob[q] 
+
+    return datapoint 
+
+#Build Labelled Dataset of Observation Doubles
+def buildLabelledDataset(n, obs, tags):
+    x = []
+    y = []
+    usedTags = []
+    ln = len(obs[0])
+    for i in range(n):
+
+        #Find Matching Double
+        while True:
+            n = random.randint(0, len(tags)-1) 
+            indices = [i for i, x in enumerate(tags) if x == tags[n]]
+            if len(indices) > 1:
+                d = [obs[indices[0]][0:ln], obs[indices[1]][0:ln]]
+                x.append(augmentAndVectorize(d) )
+                y.append(0)
+                usedTags.append(tags[n])
+                break
+
+        #Find Non-Matching Double
+        while True:
+            n2 = random.randint(0, len(tags)-1) 
+            if tags[n] != tags[n2]:
+                d = [obs[n][0:ln], obs[n2][0:ln]] 
+                x.append(augmentAndVectorize(d))
+                y.append(1)
+                usedTags.append(tags[n2])
+                break
+
+    return x, y
+    
+#Pull Training Data and Tags from input files
+def loadDataTags(file1, file2):
+
+    with open(file1, 'r') as f:
+        obs = [q.split(',') for q in f.readlines()]
+    for i in range(len(obs)):
+        obs[i] = [float(q) for q in obs[i]]
+        obs[i].append(np.linalg.norm(obs[i][-3:]))#Add Streak Length
+
+        #Refactor Observer Position Into Unit Vector and Magnitude
+        R = np.linalg.norm(obs[i][1:4])
+        obs[i][1] = obs[i][1] /R
+        obs[i][2] = obs[i][2] /R
+        obs[i][3] = obs[i][3] /R
+        obs[i].append(R)
+
+    if file2 != 'NA':
+        with open(file2, 'r') as f:
+            tags = [int(q) for q in f.readlines()]
+        
+    return obs, tags
+
+def accuracy(out, labels):
+  outputs = np.argmax(out, axis=1)
+  return np.divide(np.sum(outputs==labels),labels.size)
+
+def defineModel():
+
+    device = torch.device('cpu') #Assign to CPU
+
+    # Define layers in model
+    model = torch.nn.Sequential(
+            torch.nn.Linear(D_in, H),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(H),
+            torch.nn.Linear(H, H),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(H),
+            torch.nn.Linear(H, H),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(H),
+            torch.nn.Linear(H, H),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(H),
+            torch.nn.Linear(H, 2),
+            torch.nn.LogSoftmax()
+            ).to(device)
+
+    return model
+
+
+if __name__ == "__main__":
+
+    device = torch.device('cpu') #Assign to CPU
+
+    if TRAIN_FLAG == 1:
+
+        obs, tags = loadDataTags(train_data, train_tags) #Load Training Data
+        obs_val, tags_val = loadDataTags(val_data, val_tags) #Load Validation Data 
+
+        #Build Training Dataset
+        x, y = buildLabelledDataset(N, obs, tags)
+        print('Training Data & # RSOs Used', len(x), len(y))
+
+        #Build Validation Dataset
+        x_val, y_val = buildLabelledDataset(Nval, obs_val, tags_val)
+        print('Validation Data & # RSOs Used',  len(x_val), len(y_val))
+
+        print('/n Total RSOs', len(set(tags)))
+
+        #Assign Training and Val Data
+        x = torch.FloatTensor(x, device=device)
+        y = torch.LongTensor(y, device=device)
+        x_val = torch.FloatTensor(x_val, device=device)
+        y_val = torch.LongTensor(y_val, device=device)
+
+    #Define Model Layers
+    model = defineModel()
+
+    # Load Saved Model
+    if LOAD_SAVED_MODEL_FLAG == 1:
+        model.load_state_dict(torch.load(MODEL_FILE))
+
+    # Define Loss Function
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    #Setup optimizer
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0)
+
+    if TRAIN_FLAG == 1:
+        for t in range(NUMEPOCHS):
+
+            #Forward pass, predict label y given training data x
+            y_pred = model(x)
+
+            #Switch to Eval Mode for Validation Data and predict based on validation data x_val
+            model.eval()
+            y_pred_val = model(x_val)
+
+            #Switch Back to Train Mode
+            model.train()
+
+            # Compute the loss and accuracy
+            loss = loss_fn(y_pred, y)
+            acc_train = accuracy(y_pred.detach().cpu().numpy(), y.detach().cpu().numpy())
+            acc_val = accuracy(y_pred_val.detach().cpu().numpy(), y_val.detach().cpu().numpy())
+            print(t, 'Train Loss=', loss.item(), 'Train Acc:', acc_train, 'Val Acc: ', acc_val)
+
+            # Zero the gradients before running the backward pass.
+            model.zero_grad()
+            optimizer.zero_grad()
+
+            # Backward pass: compute gradient of the loss with respect to all the learnable parameters
+            loss.backward()
+
+            # Update Parameters
+            optimizer.step()
+
+            # Update the weights using gradient descent
+            with torch.no_grad():
+                for param in model.parameters():
+                    param.data -= learning_rate * param.grad
+
+        torch.save(model.state_dict(), MODEL_FILE) # Save model
