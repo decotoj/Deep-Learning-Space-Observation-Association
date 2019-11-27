b98f4e5b2ffb692ea7b021ed968ca814d37e3249
David RC Dayton <david.rc.dayton@gmail.com>
2019-11-26 21:46:50 -0700

    reformat

--- src/main_train_model.py
+++ src/main_train_model.py
@@ -3,10 +3,10 @@
 # Authors: Jake Decoto
 
 # Next Steps
-#1) Audit code (main and sythesize) and hone down to minimum
-#A) Reduce false positive triplets, 1) loss function for doubles penalize false+, 2) separate triplet NN for those identified by doublet NN
-#B) Separate out grading and knowledge of tags into separate callable function (so can be run as unknowing participant)
-#C) Revisit simulated data settings to ensure data is sufficiently real world problem representive
+# 1) Audit code (main and sythesize) and hone down to minimum
+# A) Reduce false positive triplets, 1) loss function for doubles penalize false+, 2) separate triplet NN for those identified by doublet NN
+# B) Separate out grading and knowledge of tags into separate callable function (so can be run as unknowing participant)
+# C) Revisit simulated data settings to ensure data is sufficiently real world problem representive
 
 # The following loads prebuilt training and test data consisting of optical observerations
 # of satellites.  A neural network is then trained to identify pairs of optical observations
@@ -14,32 +14,35 @@
 # used to identify triplets of observations in the test data that are most likely to be of
 # the same satellite.  No apriori knowledge of physics or orbital mechanics is used.
 
-import torch
 import random
-import numpy as np
-import torch.optim as optim
 from collections import defaultdict
+
+import numpy as np
+import torch
+import torch.optim as optim
 
 random.seed(654654)
 
-#Network and Data Parameters
-LOAD_SAVED_MODEL_FLAG = 1 #1=Load prior saved model, 0=Train model from scratch
-TRAIN_FLAG = 1 #1=TRAIN, 0=EVAL ONLY, 2=TRAIN BUT DONT SAVE
-MODEL_FILE = 'model.pt' #Path to saved model file if applicable, also is name of new model file to be saved
-D_in = 416 #Length of Input Vectors after Augmentation (time, observer x-y-z unit vec, observervation x-y-z unit vec, streak direction x-y-z unit vec, observer vector mag)
-H = 208 #416 #Hidden layer(s) dimension
-N = 40000#Number of training examples to create from loaded data (Baseline = 40000)
-Nval = 2500 ##Number of validation examples to create from loaded data (Baseline = 2500)
-NUMEPOCHS = 1000 #Number of Training Epochs
-learning_rate = 1e-6 #Baseline = 1e-4
-train_data = 'train_data.csv'
-train_tags = 'train_tags.csv'
-val_data = 'val_data.csv'
-val_tags = 'val_tags.csv'
+# Network and Data Parameters
+LOAD_SAVED_MODEL_FLAG = (
+    1  # 1=Load prior saved model, 0=Train model from scratch
+)
+TRAIN_FLAG = 1  # 1=TRAIN, 0=EVAL ONLY, 2=TRAIN BUT DONT SAVE
+MODEL_FILE = "model.pt"  # Path to saved model file if applicable, also is name of new model file to be saved
+D_in = 416  # Length of Input Vectors after Augmentation (time, observer x-y-z unit vec, observervation x-y-z unit vec, streak direction x-y-z unit vec, observer vector mag)
+H = 208  # 416 #Hidden layer(s) dimension
+N = 1000  # Number of training examples to create from loaded data (Baseline = 40000)
+Nval = 1000  ##Number of validation examples to create from loaded data (Baseline = 2500)
+NUMEPOCHS = 1000  # Number of Training Epochs
+learning_rate = 1e-4  # Baseline = 1e-4
+train_data = "train_data.csv"
+train_tags = "train_tags.csv"
+val_data = "val_data.csv"
+val_tags = "val_tags.csv"
+
 
 def augmentAndVectorize(ob):
-
-    #Sort Observations by Epoch and Normalize Epoch
+    # Sort Observations by Epoch and Normalize Epoch
     ob.sort(key=lambda x: x[0])
     for q in range(len(ob)):
         ob[q][0] = ob[q][0] - ob[0][0]
@@ -52,30 +55,32 @@ def augmentAndVectorize(ob):
     #             for s in [0,4,5,6,7,8,9,10,11]:
     #                 ob[z].append((ob[z][q]-ob[z-1][r])/(ob[z][s]-ob[z-1][s]))
 
-    #416 Parameters w/ ~60% Validation Accuracy (w/ 77% Train Acc) Reached by Epoch 37
-    #Add Derived Parameter from Observation Unit Vector and Streak Vector and Magnitude (416)
+    # 416 Parameters w/ ~60% Validation Accuracy (w/ 77% Train Acc) Reached by Epoch 37
+    # Add Derived Parameter from Observation Unit Vector and Streak Vector and Magnitude (416)
     for z in range(1, len(ob)):
-        for q in [4,5,6,7,8,9,10]:
-            for r in [4,5,6,7,8,9,10]:
-                for s in [0,4,5,6,7,8,9,10]:
-                    ob[z].append((ob[z][q]-ob[z-1][r])/(ob[z][s]-ob[z-1][s]))
+        for q in [4, 5, 6, 7, 8, 9, 10]:
+            for r in [4, 5, 6, 7, 8, 9, 10]:
+                for s in [0, 4, 5, 6, 7, 8, 9, 10]:
+                    ob[z].append(
+                        (ob[z][q] - ob[z - 1][r]) / (ob[z][s] - ob[z - 1][s])
+                    )
 
-    #Vectorize
+    # Vectorize
     datapoint = []
     for q in range(len(ob)):
         datapoint = datapoint + ob[q]
 
     return datapoint
 
-#Build Labelled Dataset of Observation Doubles
+
+# Build Labelled Dataset of Observation Doubles
 def buildLabelledDataset(Nc, obs, tags):
     x = []
     y = []
     while len(x) <= Nc:
-
-        #Find Matching Double
+        # Find Matching Double
         while True:
-            n = random.randint(0, len(tags)-1)
+            n = random.randint(0, len(tags) - 1)
             indices = [i for i, x in enumerate(tags) if x == tags[n]]
             if len(indices) > 1:
                 d = [obs[indices[0]][:], obs[indices[1]][:]]
@@ -83,9 +88,9 @@ def buildLabelledDataset(Nc, obs, tags):
                 y.append(0)
                 break
 
-        #Find Non-Matching Double
+        # Find Non-Matching Double
         while True:
-            n2 = random.randint(0, len(tags)-1)
+            n2 = random.randint(0, len(tags) - 1)
             if tags[n] != tags[n2]:
                 d = [obs[n][:], obs[n2][:]]
                 x.append(augmentAndVectorize(d))
@@ -94,91 +99,94 @@ def buildLabelledDataset(Nc, obs, tags):
 
     return x, y
 
-#Pull Training Data and Tags from input files
-def loadDataTags(file1, file2):
 
-    with open(file1, 'r') as f:
-        obs = [q.split(',') for q in f.readlines()]
+# Pull Training Data and Tags from input files
+def loadDataTags(file1, file2):
+    with open(file1, "r") as f:
+        obs = [q.split(",") for q in f.readlines()]
     for i in range(len(obs)):
         obs[i] = [float(q) for q in obs[i]]
-        obs[i].append(np.linalg.norm(obs[i][-3:]))#Add Streak Length
+        obs[i].append(np.linalg.norm(obs[i][-3:]))  # Add Streak Length
 
-        #Refactor Observer Position Into Unit Vector and Magnitude
+        # Refactor Observer Position Into Unit Vector and Magnitude
         R = np.linalg.norm(obs[i][1:4])
-        obs[i][1] = obs[i][1] /R
-        obs[i][2] = obs[i][2] /R
-        obs[i][3] = obs[i][3] /R
+        obs[i][1] = obs[i][1] / R
+        obs[i][2] = obs[i][2] / R
+        obs[i][3] = obs[i][3] / R
         obs[i].append(R)
 
-    if file2 != 'NA':
-        with open(file2, 'r') as f:
+    if file2 != "NA":
+        with open(file2, "r") as f:
             tags = [int(q) for q in f.readlines()]
 
     return obs, tags
 
+
 def accuracy(out, labels):
-  outputs = np.argmax(out, axis=1)
-  return np.divide(np.sum(outputs==labels),labels.size)
+    outputs = np.argmax(out, axis=1)
+    return np.divide(np.sum(outputs == labels), labels.size)
+
 
 def defineModel():
-
-    device = torch.device('cpu') #Assign to CPU
+    device = torch.device("cpu")  # Assign to CPU
 
     # Define layers in model
     model = torch.nn.Sequential(
-            torch.nn.Linear(D_in, H),
-            torch.nn.ReLU(),
-            torch.nn.BatchNorm1d(H),
-            torch.nn.Linear(H, H),
-            torch.nn.ReLU(),
-            torch.nn.BatchNorm1d(H),
-            torch.nn.Linear(H, H),
-            torch.nn.ReLU(),
-            torch.nn.BatchNorm1d(H),
-            torch.nn.Linear(H, H),
-            torch.nn.ReLU(),
-            torch.nn.BatchNorm1d(H),
-            torch.nn.Linear(H, H),
-            torch.nn.ReLU(),
-            torch.nn.BatchNorm1d(H),
-            torch.nn.Linear(H, H),
-            torch.nn.ReLU(),
-            torch.nn.BatchNorm1d(H),
-            torch.nn.Linear(H, H),
-            torch.nn.ReLU(),
-            torch.nn.BatchNorm1d(H),
-            torch.nn.Linear(H, 2),
-            torch.nn.LogSoftmax(1) #11/14/2019: Changed to (1) instead of () due to depracation warning, untested
-            ).to(device)
+        torch.nn.Linear(D_in, H),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm1d(H),
+        torch.nn.Linear(H, H),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm1d(H),
+        torch.nn.Linear(H, H),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm1d(H),
+        torch.nn.Linear(H, H),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm1d(H),
+        torch.nn.Linear(H, H),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm1d(H),
+        torch.nn.Linear(H, H),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm1d(H),
+        torch.nn.Linear(H, H),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm1d(H),
+        torch.nn.Linear(H, 2),
+        torch.nn.LogSoftmax(
+            1
+        ),  # 11/14/2019: Changed to (1) instead of () due to depracation warning, untested
+    ).to(device)
 
     return model
 
 
 if __name__ == "__main__":
-
-    device = torch.device('cpu') #Assign to CPU
+    device = torch.device("cpu")  # Assign to CPU
 
     if TRAIN_FLAG >= 1:
-
-        #Build Validation Dataset
-        obs_val, tags_val = loadDataTags(val_data, val_tags) #Load Validation Data
+        # Build Validation Dataset
+        obs_val, tags_val = loadDataTags(
+            val_data, val_tags
+        )  # Load Validation Data
         x_val, y_val = buildLabelledDataset(Nval, obs_val, tags_val)
-        print('Validation Data & # RSOs Used', Nval, len(x_val), len(y_val))
+        print("Validation Data & # RSOs Used", Nval, len(x_val), len(y_val))
 
-        #Build Training Dataset
-        obs, tags = loadDataTags(train_data, train_tags) #Load Training Data
+        # Build Training Dataset
+        obs, tags = loadDataTags(train_data, train_tags)  # Load Training Data
         x, y = buildLabelledDataset(N, obs, tags)
-        print('Training Data & # RSOs Used', N, len(x), len(y))
+        print("Training Data & # RSOs Used", N, len(x), len(y))
 
-        print('\n Total RSOs', len(set(tags)))
+        print("\n Total RSOs", len(set(tags)))
 
-        #Assign Training and Val Data
+        # Assign Training and Val Data
         x = torch.FloatTensor(x, device=device)
         y = torch.LongTensor(y, device=device)
         x_val = torch.FloatTensor(x_val, device=device)
         y_val = torch.LongTensor(y_val, device=device)
 
-    #Define Model Layers
+    # Define Model Layers
     model = defineModel()
 
     # Load Saved Model
@@ -188,37 +196,51 @@ if __name__ == "__main__":
     # Define Loss Function
     loss_fn = torch.nn.CrossEntropyLoss()
 
-    #Setup optimizer
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0)
+    # Setup optimizer
+    optimizer = optim.Adam(
+        model.parameters(), lr=learning_rate, weight_decay=0
+    )
 
     if TRAIN_FLAG >= 1:
         acc_val_best = 0
         for t in range(NUMEPOCHS):
             model.eval()
-            #Forward pass, predict label y given training data x
+            # Forward pass, predict label y given training data x
             y_pred = model(x)
 
-            #Switch to Eval Mode for Validation Data and predict based on validation data x_val
-            #model.eval()
+            # Switch to Eval Mode for Validation Data and predict based on validation data x_val
+            # model.eval()
             y_pred_val = model(x_val)
 
-            #Switch Back to Train Mode
+            # Switch Back to Train Mode
             model.train()
 
             # Compute the loss and accuracy
             loss = loss_fn(y_pred, y)
-            acc_train = accuracy(y_pred.detach().cpu().numpy(), y.detach().cpu().numpy())
+            acc_train = accuracy(
+                y_pred.detach().cpu().numpy(), y.detach().cpu().numpy()
+            )
             model.eval()
-            acc_val = accuracy(y_pred_val.detach().cpu().numpy(), y_val.detach().cpu().numpy())
+            acc_val = accuracy(
+                y_pred_val.detach().cpu().numpy(), y_val.detach().cpu().numpy()
+            )
             model.train()
-            print(t, 'Train Loss=', loss.item(), 'Train Acc:', acc_train, 'Val Acc: ', acc_val)
+            print(
+                t,
+                "Train Loss=",
+                loss.item(),
+                "Train Acc:",
+                acc_train,
+                "Val Acc: ",
+                acc_val,
+            )
 
             if acc_val > acc_val_best and TRAIN_FLAG != 2:
                 model.eval()
-                torch.save(model.state_dict(), MODEL_FILE) # Save model
+                torch.save(model.state_dict(), MODEL_FILE)  # Save model
                 model.train()
                 acc_val_best = acc_val
-                print('saved model')
+                print("saved model")
 
             # Zero the gradients before running the backward pass.
             model.zero_grad()
@@ -234,4 +256,3 @@ if __name__ == "__main__":
             with torch.no_grad():
                 for param in model.parameters():
                     param.data -= learning_rate * param.grad
-
